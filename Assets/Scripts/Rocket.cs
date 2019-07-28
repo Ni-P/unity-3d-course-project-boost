a7091a4280ecf93b764bc2b96128ed469f762b76
Niko Pinnis <niko.pinnis@gmail.com>
2019-07-28 21:09:15 +0300

    After L48

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+        Audio();
+    }
+
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // stop physics
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            transform.Rotate(Vector3.forward * Time.deltaTime * 20);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * 20);
+        }
+
+        rigidBody.freezeRotation = false;
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * 10);
+        }
+    }
+
+    private void Audio()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.volume = 0.8f;
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.volume -= 0.05f;
+                if (audioSource.volume == 0.0f)
+                    audioSource.Stop();
+            }
+        }
+    }
+}
